77ee35b7a1d8721df42b488cfac5706d87f0b548
EspytheEspeon <hd02roberth@gmail.com>
2025-08-17 16:48:03 -0500

    F: Removed hard coded link to book, added sys functions to call any book using sys.argv arguemnts

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 #get_book_text function: takes in file name, returns string
 def get_book_text(f):
     file_contents = f.read()
@@ -13,23 +19,23 @@ from stats import get_characters
 from stats import get_report
 
 #main
-with open("books/frankenstein.txt") as f:
+with open(sys.argv[1]) as f:
     book1 = get_book_text(f)
     book2 = get_words(book1)
     book3 = get_characters(book1)
     book4 = get_report(book3)
 
 #print(f"{book2} words found in the document" )
-print(f"{book2} words found in the document" )
+#print(f"{book2} words found in the document" )
 
 #print a dictionary of each character and how many times it appears in the book
 #print(book3)
 
 #print a list of the dictionary entries sorted by the value of the entries
-print(book4)
+#print(book4)
 
 print("=========== Bookbot ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {sys.argv[1]} ...")
 print("---------- Word Count ----------")
 print(f"Found {book2} total words")
 print("---------- Character Count -----")
