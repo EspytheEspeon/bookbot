7df8473b821eda440e62a4a745cdd8532e8c5123
EspytheEspeon <hd02roberth@gmail.com>
2025-08-17 13:05:09 -0500

    Created function to read characters of book and count how many times each character appears

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ def get_book_text(f):
     file_contents = f.read()
     return file_contents
 
-#Import call for the function in stats.py
+#Import call for the function in stats.py for get_words
 from stats import get_words
 
+#Import call for the function in stats.py for get_characters
+from stats import get_characters
 
 #main
 with open("books/frankenstein.txt") as f:
     book1 = get_book_text(f)
     book2 = get_words(book1)
+    book3 = get_characters(book1)
 
+#print(f"{book2} words found in the document" )
 print(f"{book2} words found in the document" )
 
+#print a dictionary of each character and how many times it appears in the book
+print(book3)
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def get_words(f):
     file_contents = f.split()   
 
     return len(file_contents)
+
+#get_characters function: takes file name, sets as a string, counts characters
+def get_characters(f):
+    characters = {}
+    file_contents = list(str.lower(f))
+    for letters in file_contents:
+        if letters not in characters:
+            characters[letters] = 1
+        else:
+            characters[letters] += 1
+    return characters
+
